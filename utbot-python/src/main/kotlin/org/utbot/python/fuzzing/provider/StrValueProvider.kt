c93f4a12b5255473b45967b09ca5ac9d22f3751a
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2023-06-15 16:36:23 +0300

    Add regexp support

--- utbot-python/src/main/kotlin/org/utbot/python/fuzzing/provider/StrValueProvider.kt
+++ utbot-python/src/main/kotlin/org/utbot/python/fuzzing/provider/StrValueProvider.kt
@@ -3,6 +3,7 @@ package org.utbot.python.fuzzing.provider
 import org.utbot.fuzzing.Seed
 import org.utbot.fuzzing.ValueProvider
 import org.utbot.fuzzing.seeds.KnownValue
+import org.utbot.fuzzing.seeds.RegexValue
 import org.utbot.fuzzing.seeds.StringValue
 import org.utbot.python.framework.api.python.PythonTree
 import org.utbot.python.framework.api.python.util.pythonStrClassId
@@ -13,6 +14,9 @@ import org.utbot.python.fuzzing.provider.utils.generateSummary
 import org.utbot.python.fuzzing.provider.utils.transformQuotationMarks
 import org.utbot.python.newtyping.general.Type
 import org.utbot.python.newtyping.pythonTypeName
+import java.util.regex.Pattern
+import java.util.regex.PatternSyntaxException
+import kotlin.random.Random
 
 object StrValueProvider : ValueProvider<Type, PythonFuzzedValue, PythonMethodDescription> {
     override fun accept(type: Type): Boolean {
@@ -23,7 +27,15 @@ object StrValueProvider : ValueProvider<Type, PythonFuzzedValue, PythonMethodDes
         return concreteValues
             .filter { accept(it.type) }
             .map {
-                StringValue((it.value as String).transformQuotationMarks())
+                val value = it.value as String
+                if (Pattern.matches("r\".*\"", value) || Pattern.matches("r'.*'", value)) {
+                    value.drop(2).dropLast(1)
+                } else {
+                    value
+                }
+            }
+            .map {
+                StringValue(it.transformQuotationMarks())
             }
     }
 
@@ -35,6 +47,17 @@ object StrValueProvider : ValueProvider<Type, PythonFuzzedValue, PythonMethodDes
             StringValue("€"),
         )
         strConstants.forEach { yieldStrings(it) { value } }
+        strConstants
+            .map { it.value }
+            .filter {
+                try {
+                    Pattern.compile(it); true
+                } catch (_: PatternSyntaxException) {
+                    false
+                }
+            }.forEach {
+                yieldStrings(RegexValue(it, Random(0)), StringValue::value)
+            }
     }
 
     private suspend fun <T : KnownValue<T>> SequenceScope<Seed<Type, PythonFuzzedValue>>.yieldStrings(value: T, block: T.() -> Any) {
